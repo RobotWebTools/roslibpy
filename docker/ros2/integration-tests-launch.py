c4aea8531c71f24c0f3e518e30a9adac307b81fd
Gonzalo Casas <casas@arch.ethz.ch>
2026-01-16 15:55:41 +0100

    Update copyright holder in LICENSE file (#148)
    * Update copyright holder in LICENSE file
    
    * Fix integration tests for ROS2
    
    * lint

--- docker/ros2/integration-tests-launch.py
+++ docker/ros2/integration-tests-launch.py
@@ -3,20 +3,22 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import ExecuteProcess, IncludeLaunchDescription
 
-def generate_launch_description():
-    return LaunchDescription([
-        # Start rosbridge_websocket
-        IncludeLaunchDescription(
-            PathJoinSubstitution([
-                FindPackageShare('rosbridge_server'),
-                'launch',
-                'rosbridge_websocket_launch.xml'
-            ])
-        ),
 
-        # Start fibonacci_server.py with python3
-        ExecuteProcess(
-            cmd=['python3', "/fibonacci_server.py"],
-            output='screen'
-        )
-    ])
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            # Start rosbridge_websocket
+            IncludeLaunchDescription(
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("rosbridge_server"),
+                        "launch",
+                        "rosbridge_websocket_launch.xml",
+                    ]
+                ),
+                launch_arguments={"delay_between_messages": "0.0"}.items(),
+            ),
+            # Start fibonacci_server.py with python3
+            ExecuteProcess(cmd=["python3", "/fibonacci_server.py"], output="screen"),
+        ]
+    )
